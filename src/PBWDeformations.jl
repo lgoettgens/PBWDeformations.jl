6ecddd6556efee83e5c7ad417f4f5eb2754184c9
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2021-05-26 17:20:32 +0200

    update ci and add dummy test

--- src/PBWDeformations.jl
+++ src/PBWDeformations.jl
@@ -1,5 +1,14 @@
 module PBWDeformations
 
-# Write your package code here.
+"""
+    add(x:Integer, y:Integer) :: Integer
+
+adds two integer numbers
+```jldoctest
+julia> PBWDeformations.add(12,30)
+42
+```
+"""
+add(x::Integer, y::Integer) = x+y
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 include("setup.jl")
 
 @testset ExtendedTestSet "All PBWDeformations tests" begin
+    @test 2 == PBWDeformations.add(1,1)
     # Write your tests here.
 end
