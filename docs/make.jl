6c783d346092e3a91086dcab73b4ae610fc6ef01
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2021-05-26 17:51:34 +0200

    fix ci

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,12 @@
-using PBWDeformations
 using Documenter
+using PBWDeformations
 
 DocMeta.setdocmeta!(PBWDeformations, :DocTestSetup, :(using PBWDeformations); recursive=true)
 
 makedocs(;
     modules=[PBWDeformations],
     authors="Johannes Flake <flake@art.rwth-aachen.de>, Lars Göttgens <lars.goettgens@rwth-aachen.de>, Phil Pützstück <phil.puetzstueck@rwth-aachen.de>",
-    repo="https://gitlab.com/a-rt/PBWDeformations.jl/blob/{commit}{path}#{line}",
+    # repo="https://gitlab.com/a-rt/PBWDeformations.jl/blob/{commit}{path}#{line}",
     sitename="PBWDeformations.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
