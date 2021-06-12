de45f5b3730082984ba4bf9c734e24a4f58baee8
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2021-06-12 18:50:32 +0200

    revert type changes

--- src/SmashProductsLie.jl
+++ src/SmashProductsLie.jl
@@ -4,45 +4,45 @@ GAP = Oscar.GAP.Globals
 toGAP = Oscar.GAP.julia_to_gap
 fromGAP = Oscar.GAP.gap_to_julia
 
-function sanitizeLieInput(dynkin::Char, n::Integer) :: Nothing
+function sanitizeLieInput(dynkin::Char, n::Int64) :: Nothing
     @assert dynkin in ['A', 'B', 'C', 'D']
     if dynkin == 'A'
         @assert n >= 1
-    elseif dynkin == 'B' || dykin == 'C'
+    elseif dynkin == 'B' || dynkin == 'C'
         @assert n >= 2
     elseif dynkin == 'D'
         @assert n >= 4
     end
 end
 
-BasisElement = Pair{Symbol, Integer}
+BasisElement = Tuple{Symbol, Int64}
 Coefficient = Integer
 
-lie(i::Integer) = (:lie, i) :: BasisElement
-mod(i::Integer) = (:mod, i) :: BasisElement
+lie(i::Int64) = (:lie, i) :: BasisElement
+mod(i::Int64) = (:mod, i) :: BasisElement
 
 struct SmashProductLie <: SmashProduct
-    nL :: Integer
-    nV :: Integer
+    nL :: Int64
+    nV :: Int64
     ```
     Contains the simplified commutator of two elements as a linear combination.
     An empty list thus is the empty sum and means that the two elements commutate.
     An absent entry means that there is only a formal commutator.
     ```
-    commTable :: Dict{Pair{BasisElement, BasisElement}, Vector{Pair{Coefficient, BasisElement}}}
+    commTable :: Dict{Tuple{BasisElement, BasisElement}, Vector{Tuple{Coefficient, BasisElement}}}
 
-    function SmashProductLie(dynkin::Char, n::Integer, lambda::Vector{Integer})
+    function SmashProductLie(dynkin::Char, n::Int64, lambda::Vector{Int64})
         @assert n == length(lambda)
         sanitizeLieInput(dynkin, n)
 
-        commTable = Dict{Pair{BasisElement, BasisElement}, Vector{Pair{Coefficient, BasisElement}}}()
+        commTable = Dict{Tuple{BasisElement, BasisElement}, Vector{Tuple{Coefficient, BasisElement}}}()
             # (lie(i), lie(j)) => [(c, lie(k))]
             # (lie(i), mod(j)) => [(c, mod(k))]
 
         L = GAP.SimpleLieAlgebra(toGAP(string(dynkin)), n, GAP.Rationals)
         nL = GAP.Dimension(L)
         commTableL = fromGAP(GAP.StructureConstantsTable(GAP.Basis(L)))[1:nL]
-        
+
         for i in 1:nL, j in 1:i-1
             commTable[(lie(i), lie(j))] = [(c, lie(k)) for (k, c) in zip(commTableL[i][j]...)]
         end
