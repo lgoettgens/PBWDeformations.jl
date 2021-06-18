bc8fe684e287ead8cbc0a7bb09b25c2aa555e65a
Phil Pützstück <phil.puetzstueck@rwth-aachen.de>
2021-06-19 00:25:16 +0200

    add non-symmetric deformations

--- src/SmashProductDeformLie.jl
+++ src/SmashProductDeformLie.jl
@@ -1,25 +1,53 @@
-struct SmashProductSymmDeformLie
+struct SmashProductDeformLie
     sp :: SmashProductLie
+    symmetric :: Bool
 end
 
-function Base.show(io::IO, spd::SmashProductSymmDeformLie)
-    println(io, "Symmetric deformation of:")
+function Base.show(io::IO, spd::SmashProductDeformLie)
+    if spd.symmetric
+        println(io, "Symmetric deformation of:")
+    else
+        println(io, "Deformation of:")
+    end
     show(spd.sp)
 end
 
 
-function smashProductSymmDeformLie(dynkin::Char, n::Int64, lambda::Vector{Int64}) :: QuadraticAlgebra{SmashProductSymmDeformLie}
-    @assert n == length(lambda)
-    sanitizeLieInput(dynkin, n)
+function smashProductDeformLie(sp::QuadraticAlgebra{SmashProductLie}, kappa::Matrix{AlgebraElement}) :: QuadraticAlgebra{SmashProductDeformLie}
+    nV = sp.extraData.nV
+    @assert size(kappa) == (nV, nV)
+    # @assert all(map(e -> e in sp, kappa))
 
-    sp = smashProductLie(dynkin, n, lambda)
-    basis = sp.basis
     relTable = sp.relTable
-    extraData = SmashProductSymmDeformLie(sp.extraData)
+    symmetric = true
+
+    for i in 1:nV, j in 1:i-1
+        if symmetric && kappa[i,j] != [(1, [mod(j), mod(i)])]
+            symmetric = false
+        end
+
+        relTable[(mod(i), mod(j))] = kappa[i,j]
+    end
+
+    extraData = SmashProductDeformLie(sp.extraData, symmetric)
+    QuadraticAlgebra{SmashProductDeformLie}(sp.basis, relTable, extraData)
+end
+
+
+function smashProductSymmDeformLie(sp::QuadraticAlgebra{SmashProductLie}) :: QuadraticAlgebra{SmashProductDeformLie}
+    relTable = sp.relTable
 
     for i in 1:sp.extraData.nV, j in 1:i-1
         relTable[(mod(i), mod(j))] = [(1, [mod(j), mod(i)])]
     end
 
-    QuadraticAlgebra{SmashProductSymmDeformLie}(basis, relTable, extraData)
+    extraData = SmashProductDeformLie(sp.extraData, true)
+    QuadraticAlgebra{SmashProductDeformLie}(sp.basis, relTable, extraData)
+end
+
+function smashProductSymmDeformLie(dynkin::Char, n::Int64, lambda::Vector{Int64}) :: QuadraticAlgebra{SmashProductDeformLie}
+    @assert n == length(lambda)
+    sanitizeLieInput(dynkin, n)
+
+    smashProductSymmDeformLie(smashProductLie(dynkin, n, lambda))
 end
