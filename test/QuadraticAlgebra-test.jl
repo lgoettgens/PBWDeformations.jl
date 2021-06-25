76a1db1e7acc73c1b3e9c2a2706ff8a766bea83f
Phil Pützstück <phil.puetzstueck@rwth-aachen.de>
2021-06-25 23:13:41 +0200

    add tests for multiplication and exponentiation in normalForm

--- test/QuadraticAlgebra-test.jl
+++ test/QuadraticAlgebra-test.jl
@@ -7,24 +7,41 @@ dimRandomTests = [3, 10, 25, 100]
             basis = [(:basis, i) for i in 1:n]
             relTable = Dict()
             alg = PD.QuadraticAlgebra{Nothing}(basis, relTable, nothing)
+            x = alg.x
 
             for _ in 1:numRandomTests
                 ind = shuffle(rand(1:n, rand(1:2n)))
                 if length(ind) > 0
-                    @test PD.normalForm(alg, alg.x(ind...)) == alg.x(ind...)
+                    @test PD.normalForm(alg, prod(map(x, ind))) == PD.normalForm(alg, x(ind...)) == x(ind...)
                 end
             end
+        end
+
+        @testset "products and exponentiation" begin
+            basis = [(:basis, i) for i in 1:10]
+            relTable = Dict()
+            alg = PD.QuadraticAlgebra{Nothing}(basis, relTable, nothing)
+            x = alg.x
+
+            @test PD.normalForm(alg, x(1,2)*x(3,4)) == x(1,2,3,4)
+            @test PD.normalForm(alg, (x(1) + x(1,2))*x(3,4)) == x(1,3,4) + x(1,2,3,4)
+            @test PD.normalForm(alg, x(1)*(x(2) + x(3,4))*x(5,6)) == x(1,2,5,6) + x(1,3,4,5,6)
+
+            @test PD.normalForm(alg, x(1)^2*x(2)*x(3)^2) == x(1,1,2,3,3)
+            @test PD.normalForm(alg, (x(1)+x(2))^2) == x(1,1) + x(1,2) + x(2,1) + x(2,2)
+            @test PD.normalForm(alg, x(1)^5) == x(1,1,1,1,1)
         end
 
         @testset "symmetric algebra over V with dim V = $n" for n in dimRandomTests
             basis = [(:symm, i) for i in 1:n]
             relTable = Dict([((basis[i], basis[j]), [(1, [basis[j], basis[i]])]) for i in 1:n for j in 1:i-1])
             alg = PD.QuadraticAlgebra{Nothing}(basis, relTable, nothing)
+            x = alg.x
 
             for _ in 1:numRandomTests
                 ind = shuffle(rand(1:n, rand(1:2n)))
                 if length(ind) > 0
-                    @test PD.normalForm(alg, alg.x(ind...)) == alg.x(sort(ind)...)
+                    @test PD.normalForm(alg, x(ind...)) == x(sort(ind)...)
                 end
             end
         end
@@ -36,15 +53,16 @@ dimRandomTests = [3, 10, 25, 100]
                 [((basis[i], basis[i]), []) for i in 1:n]...,
             ])
             alg = PD.QuadraticAlgebra{Nothing}(basis, relTable, nothing)
+            x = alg.x
 
             for _ in 1:numRandomTests
                 ind = shuffle(rand(1:n, rand(1:2n)))
                 if length(ind) > 0
                     uniqInd = unique(ind)
                     if length(unique(ind)) < length(ind)
-                        @test PD.normalForm(alg, alg.x(ind...)) == sympify(0)
+                        @test PD.normalForm(alg, x(ind...)) == sympify(0)
                     end
-                    @test PD.normalForm(alg, alg.x(uniqInd...)) == levicivita(sortperm(uniqInd)) * alg.x(sort(uniqInd)...)
+                    @test PD.normalForm(alg, x(uniqInd...)) == levicivita(sortperm(uniqInd)) * x(sort(uniqInd)...)
                 end
             end
         end
@@ -54,34 +72,35 @@ dimRandomTests = [3, 10, 25, 100]
     @testset "normalForm for smashProductLie" begin
         @testset "A_2 with hw [1,1]" begin
             sp = PD.smashProductLie('A', 2, [1,1])
+            x = sp.x
 
             # Lie elements commute as usual
-            @test PD.normalForm(sp, sp.x(8+7, 8+1) - sp.x(8+1, 8+7)) == 2*sp.x(8+1)     # [h_1,x_1] = 2x_1
-            @test PD.normalForm(sp, sp.x(8+7, 8+2) - sp.x(8+2, 8+7)) == (-1)*sp.x(8+2)  # [h_1,x_2] = -x_1
-            @test PD.normalForm(sp, sp.x(8+7, 8+3) - sp.x(8+3, 8+7)) == sp.x(8+3)     # [h_1,x_3] =  x_1
-            @test PD.normalForm(sp, sp.x(8+1, 8+4) - sp.x(8+4, 8+1)) == sp.x(8+7)     # [x_1,y_1] = h_1
+            @test PD.normalForm(sp, x(8+7, 8+1) - x(8+1, 8+7)) == 2*x(8+1)     # [h_1,x_1] = 2x_1
+            @test PD.normalForm(sp, x(8+7, 8+2) - x(8+2, 8+7)) == (-1)*x(8+2)  # [h_1,x_2] = -x_1
+            @test PD.normalForm(sp, x(8+7, 8+3) - x(8+3, 8+7)) == x(8+3)     # [h_1,x_3] =  x_1
+            @test PD.normalForm(sp, x(8+1, 8+4) - x(8+4, 8+1)) == x(8+7)     # [x_1,y_1] = h_1
 
             # Module elements do not commute at all
-            @test PD.normalForm(sp, sp.x(1, 2)) == sp.x(1, 2)
-            @test PD.normalForm(sp, sp.x(2, 1)) == sp.x(2, 1)
-            @test PD.normalForm(sp, sp.x(5, 1)) == sp.x(5, 1)
-            @test PD.normalForm(sp, sp.x(8, 4)) == sp.x(8, 4)
-            @test PD.normalForm(sp, sp.x(1, 5, 8, 4, 2)) == sp.x(1, 5, 8, 4, 2)
+            @test PD.normalForm(sp, x(1, 2)) == x(1, 2)
+            @test PD.normalForm(sp, x(2, 1)) == x(2, 1)
+            @test PD.normalForm(sp, x(5, 1)) == x(5, 1)
+            @test PD.normalForm(sp, x(8, 4)) == x(8, 4)
+            @test PD.normalForm(sp, x(1, 5, 8, 4, 2)) == x(1, 5, 8, 4, 2)
 
             # Application commutators
-            @test PD.normalForm(sp, sp.x(8+1, 1) - sp.x(1, 8+1)) == sympify(0)
-            @test PD.normalForm(sp, sp.x(8+2, 1) - sp.x(1, 8+2)) == sympify(0)
-            @test PD.normalForm(sp, sp.x(8+3, 1) - sp.x(1, 8+3)) == sympify(0)
-            @test PD.normalForm(sp, sp.x(8+4, 1) - sp.x(1, 8+4)) == sp.x(2)
-            @test PD.normalForm(sp, sp.x(8+5, 1) - sp.x(1, 8+5)) == sp.x(3)
-            @test PD.normalForm(sp, sp.x(8+6, 1) - sp.x(1, 8+6)) == sp.x(5)
-            @test PD.normalForm(sp, sp.x(8+7, 1) - sp.x(1, 8+7)) == sp.x(1)
-            @test PD.normalForm(sp, sp.x(8+8, 1) - sp.x(1, 8+8)) == sp.x(1)
-            @test PD.normalForm(sp, sp.x(8+4, 3) - sp.x(3, 8+4)) == sp.x(4)
-            @test PD.normalForm(sp, sp.x(8+5, 2) - sp.x(2, 8+5)) == sp.x(4) - sp.x(5)
+            @test PD.normalForm(sp, x(8+1, 1) - x(1, 8+1)) == sympify(0)
+            @test PD.normalForm(sp, x(8+2, 1) - x(1, 8+2)) == sympify(0)
+            @test PD.normalForm(sp, x(8+3, 1) - x(1, 8+3)) == sympify(0)
+            @test PD.normalForm(sp, x(8+4, 1) - x(1, 8+4)) == x(2)
+            @test PD.normalForm(sp, x(8+5, 1) - x(1, 8+5)) == x(3)
+            @test PD.normalForm(sp, x(8+6, 1) - x(1, 8+6)) == x(5)
+            @test PD.normalForm(sp, x(8+7, 1) - x(1, 8+7)) == x(1)
+            @test PD.normalForm(sp, x(8+8, 1) - x(1, 8+8)) == x(1)
+            @test PD.normalForm(sp, x(8+4, 3) - x(3, 8+4)) == x(4)
+            @test PD.normalForm(sp, x(8+5, 2) - x(2, 8+5)) == x(4) - x(5)
 
             # Some more complicated
-            @test PD.normalForm(sp, sp.x(8+7, 8+1, 8+2, 8+3)) == 2*sp.x(8+1, 8+2, 8+3) + sp.x(8+1, 8+2, 8+3, 8+7)
+            @test PD.normalForm(sp, x(8+7, 8+1, 8+2, 8+3)) == 2*x(8+1, 8+2, 8+3) + x(8+1, 8+2, 8+3, 8+7)
         end
 
     end
@@ -89,35 +108,36 @@ dimRandomTests = [3, 10, 25, 100]
     @testset "normalForm for smashProductSymmDeformLie" begin
         @testset "A_2 with hw [1,1]" begin
             sp = PD.smashProductSymmDeformLie('A', 2, [1,1])
+            x = sp.x
 
             # Lie elements commute as usual
-            @test PD.normalForm(sp, sp.x(8+7, 8+1) - sp.x(8+1, 8+7)) == 2*sp.x(8+1)     # [h_1,x_1] = 2x_1
-            @test PD.normalForm(sp, sp.x(8+7, 8+2) - sp.x(8+2, 8+7)) == (-1)*sp.x(8+2)  # [h_1,x_2] = -x_1
-            @test PD.normalForm(sp, sp.x(8+7, 8+3) - sp.x(8+3, 8+7)) == sp.x(8+3)     # [h_1,x_3] =  x_1
-            @test PD.normalForm(sp, sp.x(8+1, 8+4) - sp.x(8+4, 8+1)) == sp.x(8+7)     # [x_1,y_1] = h_1
+            @test PD.normalForm(sp, x(8+7, 8+1) - x(8+1, 8+7)) == 2*x(8+1)     # [h_1,x_1] = 2x_1
+            @test PD.normalForm(sp, x(8+7, 8+2) - x(8+2, 8+7)) == (-1)*x(8+2)  # [h_1,x_2] = -x_1
+            @test PD.normalForm(sp, x(8+7, 8+3) - x(8+3, 8+7)) == x(8+3)     # [h_1,x_3] =  x_1
+            @test PD.normalForm(sp, x(8+1, 8+4) - x(8+4, 8+1)) == x(8+7)     # [x_1,y_1] = h_1
 
             # Module elements do commute
-            @test PD.normalForm(sp, sp.x(1, 2)) == sp.x(1, 2)
-            @test PD.normalForm(sp, sp.x(2, 1)) == sp.x(1, 2)
-            @test PD.normalForm(sp, sp.x(5, 1)) == sp.x(1, 5)
-            @test PD.normalForm(sp, sp.x(8, 4)) == sp.x(4, 8)
-            @test PD.normalForm(sp, sp.x(1, 5, 8, 4, 2)) == sp.x(1, 2, 4, 5, 8)
+            @test PD.normalForm(sp, x(1, 2)) == x(1, 2)
+            @test PD.normalForm(sp, x(2, 1)) == x(1, 2)
+            @test PD.normalForm(sp, x(5, 1)) == x(1, 5)
+            @test PD.normalForm(sp, x(8, 4)) == x(4, 8)
+            @test PD.normalForm(sp, x(1, 5, 8, 4, 2)) == x(1, 2, 4, 5, 8)
             
 
             # Application commutators
-            @test PD.normalForm(sp, sp.x(8+1, 1) - sp.x(1, 8+1)) == sympify(0)
-            @test PD.normalForm(sp, sp.x(8+2, 1) - sp.x(1, 8+2)) == sympify(0)
-            @test PD.normalForm(sp, sp.x(8+3, 1) - sp.x(1, 8+3)) == sympify(0)
-            @test PD.normalForm(sp, sp.x(8+4, 1) - sp.x(1, 8+4)) == sp.x(2)
-            @test PD.normalForm(sp, sp.x(8+5, 1) - sp.x(1, 8+5)) == sp.x(3)
-            @test PD.normalForm(sp, sp.x(8+6, 1) - sp.x(1, 8+6)) == sp.x(5)
-            @test PD.normalForm(sp, sp.x(8+7, 1) - sp.x(1, 8+7)) == sp.x(1)
-            @test PD.normalForm(sp, sp.x(8+8, 1) - sp.x(1, 8+8)) == sp.x(1)
-            @test PD.normalForm(sp, sp.x(8+4, 3) - sp.x(3, 8+4)) == sp.x(4)
-            @test PD.normalForm(sp, sp.x(8+5, 2) - sp.x(2, 8+5)) == sp.x(4) - sp.x(5)
+            @test PD.normalForm(sp, x(8+1, 1) - x(1, 8+1)) == sympify(0)
+            @test PD.normalForm(sp, x(8+2, 1) - x(1, 8+2)) == sympify(0)
+            @test PD.normalForm(sp, x(8+3, 1) - x(1, 8+3)) == sympify(0)
+            @test PD.normalForm(sp, x(8+4, 1) - x(1, 8+4)) == x(2)
+            @test PD.normalForm(sp, x(8+5, 1) - x(1, 8+5)) == x(3)
+            @test PD.normalForm(sp, x(8+6, 1) - x(1, 8+6)) == x(5)
+            @test PD.normalForm(sp, x(8+7, 1) - x(1, 8+7)) == x(1)
+            @test PD.normalForm(sp, x(8+8, 1) - x(1, 8+8)) == x(1)
+            @test PD.normalForm(sp, x(8+4, 3) - x(3, 8+4)) == x(4)
+            @test PD.normalForm(sp, x(8+5, 2) - x(2, 8+5)) == x(4) - x(5)
             
             # Some more complicated
-            @test PD.normalForm(sp, sp.x(8+7, 8+1, 8+2, 8+3)) == 2*sp.x(8+1, 8+2, 8+3) + sp.x(8+1, 8+2, 8+3, 8+7)
+            @test PD.normalForm(sp, x(8+7, 8+1, 8+2, 8+3)) == 2*x(8+1, 8+2, 8+3) + x(8+1, 8+2, 8+3, 8+7)
         end
 
     end
