1160dbdb1383a8726575931d1c8d48bdc6fe5a43
Johannes Flake <johannes.flake@gmail.com>
2021-09-02 13:38:12 +0000

    Update misc/lgs2.jl

--- misc/lgs2.jl
+++ misc/lgs2.jl
@@ -11,12 +11,12 @@ flush(stdout)
 
 solEx = []
 for j = 1:sol.n
-v = sol[:,j]
-av = abs.(v)
-v ./= minimum(av[av.>=1e-6])
-v = (x->rationalize(x, tol=1e-4)).(v)
-if sum(v.<0) > sum(v.>0) v=-v end
-#v = myrat(v, factorial(maximum(dgs))*2^(maximum(dgs)))
-#v = (x->round(x, sigdigits=6)).(v)
-push!(solEx, sum( v[i]*x(m...) for (i,m) in enumerate(mons) ))
+    v = sol[:,j]
+    av = abs.(v)
+    v ./= minimum(av[av.>=1e-6])
+    v = (x->rationalize(x, tol=1e-4)).(v)
+    if sum(v.<0) > sum(v.>0) v=-v end
+    #v = myrat(v, factorial(maximum(dgs))*2^(maximum(dgs)))
+    #v = (x->round(x, sigdigits=6)).(v)
+    push!(solEx, sum( v[i]*x(m...) for (i,m) in enumerate(mons) ))
 end
