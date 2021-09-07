1c9b7ba6558b7d97c924f0aa6373b72565115c15
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2021-09-07 19:56:06 +0200

    make iterator stuff work a bit more
    old stuff still there for a benchmark

--- src/SmashProductDeformLie.jl
+++ src/SmashProductDeformLie.jl
@@ -111,20 +111,18 @@ function Base.iterate(eqs::PBWDeformEqs{C}, s=nothing) where C
 
     if s[1] == 1
         i,j = res[1]
-        #h = AlgebraElement{C}(lie(s[2]; C), eqs.one)
-        # eq = (sum([c*kappa[m[1][2],j] for (c, m) in normalForm(eqs.d, comm(h, mod(i; C)))]; init=AlgebraElement{C}()) # κ([h⋅v_i,v_j])
-        #     + sum([c*kappa[i,m[1][2]] for (c, m) in normalForm(eqs.d, comm(h, mod(j; C)))]; init=AlgebraElement{C}()) # κ([v_i,h⋅v_j])
-        #     - normalForm(eqs.d, comm(h, kappa[i,j])))
-        eq = (i, j, s[2])
+        h = AlgebraElement{C}(lie(s[2]; C), eqs.one)
+        eq = (sum([c*kappa[m[1][2],j] for (c, m) in normalForm(eqs.d, comm(h, mod(i; C)))]; init=AlgebraElement{C}()) # κ([h⋅v_i,v_j])
+            + sum([c*kappa[i,m[1][2]] for (c, m) in normalForm(eqs.d, comm(h, mod(j; C)))]; init=AlgebraElement{C}()) # κ([v_i,h⋅v_j])
+            - normalForm(eqs.d, comm(h, kappa[i,j])))                                                                 # h⋅κ([v_i,v_j])
     else
         i,j,k = res[1]
-        # eq = (kappa[i,j]*mod(k; C) - mod(i; C)*kappa[j,k]
-        #     + kappa[j,k]*mod(i; C) - mod(j; C)*kappa[k,i]
-        #     + kappa[k,i]*mod(j; C) - mod(k; C)*kappa[i,j]
-        #     - kappa[k,j]*mod(i; C) + mod(k; C)*kappa[j,i]
-        #     - kappa[j,i]*mod(k; C) + mod(j; C)*kappa[i,k]
-        #     - kappa[i,k]*mod(j; C) + mod(i; C)*kappa[k,j])
-        eq = (i, j, k)
+        eq = (kappa[i,j]*mod(k; C) - mod(i; C)*kappa[j,k]
+            + kappa[j,k]*mod(i; C) - mod(j; C)*kappa[k,i]
+            + kappa[k,i]*mod(j; C) - mod(k; C)*kappa[i,j]
+            - kappa[k,j]*mod(i; C) + mod(k; C)*kappa[j,i]
+            - kappa[j,i]*mod(k; C) + mod(j; C)*kappa[i,k]
+            - kappa[i,k]*mod(j; C) + mod(i; C)*kappa[k,j])
     end
 
     return (eq, s)
@@ -148,7 +146,7 @@ function PBWDeformEqs1(d::QuadraticAlgebra{C, SmashProductDeformLie{C}}, one=C(1
     eqs = [(sum([c*kappa[m[1][2],j] for (c, m) in normalForm(d, comm(h, mod(i; C)))]; init=AlgebraElement{C}()) # κ([h⋅v_i,v_j])
           + sum([c*kappa[i,m[1][2]] for (c, m) in normalForm(d, comm(h, mod(j; C)))]; init=AlgebraElement{C}()) # κ([v_i,h⋅v_j])
           - normalForm(d, comm(h, kappa[i,j])))                                                                 # h⋅κ([v_i,v_j])
-        for i in 1:nV for j in i+1:nV for h in map(b -> AlgebraElement{C}(b, one), lie(1:nL; C))]
+        for h in map(b -> AlgebraElement{C}(b, one), lie(1:nL; C)) for i in 1:nV for j in i+1:nV]
     # m[1][2] denotes the index of the only basis element in the monomial m
 
     ## (b) trivial
@@ -174,75 +172,115 @@ end
 # end
 
 
-# function paramDeformNumberVars(nL::Int64, nV::Int64, maxdeg::Int64) :: Tuple{Int64, Int64, Int64}
-#     nKappaEntries = div(nV*(nV-1), 2)
-#     nEntryCoeffs = sum(multichoose(nL, k) for k in 0:maxdeg)
+function paramDeformNumberVars(nL::Int64, nV::Int64, maxdeg::Int64) :: Tuple{Int64, Int64, Int64}
+    nKappaEntries = div(nV*(nV-1), 2)
+    nEntryCoeffs = sum(multichoose(nL, k) for k in 0:maxdeg)
     
-#     return nKappaEntries * nEntryCoeffs, nKappaEntries, nEntryCoeffs
-# end
+    return nKappaEntries * nEntryCoeffs, nKappaEntries, nEntryCoeffs
+end
 
-# function paramDeformVars(nL::Int64, nV::Int64, maxdeg::Int64) :: Vector{String}
-#     # format: "c_{i,j,deg,[inds]}"
-#     return ["c_{$i,$j,$deg,$(isempty(inds) ? "[]" : inds)}" for i in 1:nV for j in i+1:nV for deg in 0:maxdeg for inds=multicombinations(1:nL, deg)]
-# end
+function paramDeformVars(nL::Int64, nV::Int64, maxdeg::Int64) :: Vector{String}
+    # format: "c_{i,j,deg,[inds]}"
+    return ["c_{$i,$j,$deg,$(isempty(inds) ? "[]" : inds)}" for i in 1:nV for j in i+1:nV for deg in 0:maxdeg for inds=multicombinations(1:nL, deg)]
+end
 
-# function sortVars(vars::Vector{T}, nL, nV, maxdeg) :: Matrix{Vector{Vector{T}}} where T
-#     _, nKappaEntries, nEntryCoeffs = paramDeformNumberVars(nL, nV, maxdeg)
-#     m = fill(Vector{T}[], nV, nV)
-#     k = 0
-#     for i in 1:nV, j in i+1:nV
-#         offset = 0
-#         m[i,j] = fill(T[], maxdeg+1)
-#         for d in 0:maxdeg
-#             curr = multichoose(nL, d)
-#             m[i,j][d+1] = vars[k*nEntryCoeffs+1+offset : k*nEntryCoeffs+offset+curr]
-#             offset += curr
-#         end
-#         k += 1
-#     end
-#     return m
-# end
+function sortVars(vars::Vector{T}, nL, nV, maxdeg) :: Matrix{Vector{Vector{T}}} where T
+    _, nKappaEntries, nEntryCoeffs = paramDeformNumberVars(nL, nV, maxdeg)
+    m = fill(Vector{T}[], nV, nV)
+    k = 0
+    for i in 1:nV, j in i+1:nV
+        offset = 0
+        m[i,j] = fill(T[], maxdeg+1)
+        for d in 0:maxdeg
+            curr = multichoose(nL, d)
+            m[i,j][d+1] = vars[k*nEntryCoeffs+1+offset : k*nEntryCoeffs+offset+curr]
+            offset += curr
+        end
+        k += 1
+    end
+    return m
+end
 
-# function varietyOfPBWDeforms(sp::QuadraticAlgebra{Rational{Int64}, SmashProductLie}, maxdeg::Int64) :: Tuple{Vector{MPolyElem}, MPolyRing}
-#     nL = sp.extraData.nL
-#     nV = sp.extraData.nV
+function varietyOfPBWDeforms(sp::QuadraticAlgebra{Rational{Int64}, SmashProductLie}, maxdeg::Int64) #:: Tuple{Vector{MPolyElem}, MPolyRing}
+    nL = sp.extraData.nL
+    nV = sp.extraData.nV
   
 
-#     R, vars = PolynomialRing(QQ, paramDeformVars(nL, nV, maxdeg))
+    R, vars = PolynomialRing(QQ, paramDeformVars(nL, nV, maxdeg))
 
-#     varMatrix = sortVars(vars, nL, nV, maxdeg)
+    varMatrix = sortVars(vars, nL, nV, maxdeg)
 
-#     kappa = fill(AlgebraElement{MPolyElem}(0), nV, nV)
-#     for i in 1:nV, j in i+1:nV, d in 0:maxdeg, (k, ind) in enumerate(multicombinations(1:nL, d))
-#         kappa[i,j] += varMatrix[i,j][d+1][k]*lie(ind; C=MPolyElem)
-#         kappa[j,i] -= varMatrix[i,j][d+1][k]*lie(ind; C=MPolyElem)
-#     end
+    kappa = fill(AlgebraElement{MPolyElem}(0), nV, nV)
+    for i in 1:nV, j in i+1:nV, d in 0:maxdeg, (k, ind) in enumerate(multicombinations(1:nL, d))
+        kappa[i,j] += varMatrix[i,j][d+1][k]*lie(ind; C=MPolyElem)
+        kappa[j,i] -= varMatrix[i,j][d+1][k]*lie(ind; C=MPolyElem)
+    end
 
-#     newBasis = [changeC(MPolyElem, b) for b in sp.basis]
-#     newRelTable = Dict([(changeC(MPolyElem, b1), changeC(MPolyElem, b2)) => 
-#         AlgebraElement{MPolyElem}(map(x -> (R(x[1]), changeC(MPolyElem, x[2])), unpack(a))) 
-#         for ((b1, b2), a) in pairs(sp.relTable)])
-#     newSp = QuadraticAlgebra{MPolyElem, SmashProductLie}(newBasis, newRelTable, sp.extraData)
+    newBasis = [changeC(MPolyElem, b) for b in sp.basis]
+    newRelTable = Dict([(changeC(MPolyElem, b1), changeC(MPolyElem, b2)) => 
+        AlgebraElement{MPolyElem}(map(x -> (R(x[1]), changeC(MPolyElem, x[2])), unpack(a))) 
+        for ((b1, b2), a) in pairs(sp.relTable)])
+    newSp = QuadraticAlgebra{MPolyElem, SmashProductLie}(newBasis, newRelTable, sp.extraData)
 
-#     deform = smashProductDeformLie(newSp, kappa, R(1))
+    deform = smashProductDeformLie(newSp, kappa, R(1))
 
-#     return simplifyGens(coefficientComparison(PBWDeformEqs(deform, R(1)))), R
-# end
+    return unique(vcat([map(simplifyGen, coefficientComparison(a)) for a in PBWDeformEqs{MPolyElem}(deform, R(1))]...)), R
+end
 
-# function coefficientComparison(eqs::Vector{AlgebraElement{C}}) :: Vector{C} where C
-#     result = C[]
-#     for eq in eqs
-#         # coeffcient comparison
-#         for summand in unpack(eq)
-#             (c, m) = summand
-#             push!(result, c)
-#         end
-#     end
-#     return result
-# end
+function coefficientComparison(eq::AlgebraElement{C}) :: Vector{C} where C
+    result = C[]
+    for summand in unpack(eq)
+        (c, m) = summand
+        push!(result, c)
+    end
+    return result
+end
 
-# function simplifyGens(gens::Vector{C}) :: Vector{C} where C
-#     gens = [(1//content(gen))*gen for gen in gens]
-#     gens = [(leading_coefficient(gen) < 0 ? -gen : gen) for gen in gens]
-#     return unique(gens)
-# end
+function simplifyGen(gen::MPolyElem) :: MPolyElem
+    return ((leading_coefficient(gen) < 0 ? -1 : 1)//content(gen))*gen
+end
+
+
+function varietyOfPBWDeforms1(sp::QuadraticAlgebra{Rational{Int64}, SmashProductLie}, maxdeg::Int64) #:: Tuple{Vector{MPolyElem}, MPolyRing}
+    nL = sp.extraData.nL
+    nV = sp.extraData.nV
+
+
+    R, vars = PolynomialRing(QQ, paramDeformVars(nL, nV, maxdeg))
+
+    varMatrix = sortVars(vars, nL, nV, maxdeg)
+
+    kappa = fill(AlgebraElement{MPolyElem}(0), nV, nV)
+    for i in 1:nV, j in i+1:nV, d in 0:maxdeg, (k, ind) in enumerate(multicombinations(1:nL, d))
+        kappa[i,j] += varMatrix[i,j][d+1][k]*lie(ind; C=MPolyElem)
+        kappa[j,i] -= varMatrix[i,j][d+1][k]*lie(ind; C=MPolyElem)
+    end
+
+    newBasis = [changeC(MPolyElem, b) for b in sp.basis]
+    newRelTable = Dict([(changeC(MPolyElem, b1), changeC(MPolyElem, b2)) =>
+        AlgebraElement{MPolyElem}(map(x -> (R(x[1]), changeC(MPolyElem, x[2])), unpack(a)))
+        for ((b1, b2), a) in pairs(sp.relTable)])
+    newSp = QuadraticAlgebra{MPolyElem, SmashProductLie}(newBasis, newRelTable, sp.extraData)
+
+    deform = smashProductDeformLie(newSp, kappa, R(1))
+
+    return simplifyGens1(coefficientComparison1(PBWDeformEqs1(deform, R(1)))), R
+end
+
+function coefficientComparison1(eqs::Vector{AlgebraElement{C}}) :: Vector{C} where C
+    result = C[]
+    for eq in eqs
+        # coeffcient comparison
+        for summand in unpack(eq)
+            (c, m) = summand
+            push!(result, c)
+        end
+    end
+    return result
+end
+
+function simplifyGens1(gens::Vector{C}) :: Vector{C} where C
+    gens = [(1//content(gen))*gen for gen in gens]
+    gens = [(leading_coefficient(gen) < 0 ? -gen : gen) for gen in gens]
+    return unique(gens)
+end
