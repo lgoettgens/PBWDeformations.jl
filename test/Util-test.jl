0d445b09cc5edaaf774317076d304176b6f2fa07
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2021-12-05 21:03:50 +0100

    add missing cornercase tests for Util

--- test/Util-test.jl
+++ test/Util-test.jl
@@ -2,6 +2,8 @@
     @testset "flatten" begin
         flatten = PD.flatten
 
+        @test flatten([[]]) == []
+        @test flatten(Vector{Any}[]) == []
         @test flatten([[1],[],[2,3,4],[5],[]]) == 1:5
         @test flatten([[i] for i in 1:10]) == 1:10
         @test flatten([[i,i+1] for i in 1:2:10]) == 1:10
@@ -12,6 +14,7 @@
     @testset "groupBy" begin
         groupBy = PD.groupBy
 
+        @test groupBy([]) == []
         @test groupBy([1,1,2,1]) == [[1,1],[2],[1]]
         @test groupBy([1,1,2,2,2,2,3,1,4,4]) == [[1,1],[2,2,2,2],[3],[1],[4,4]]
         @test groupBy([1 for _ in 1:10]) == [[1 for _ in 1:10]]
