bfa92d7a79f5e4878c56b8768bdd118ea8e75978
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2021-12-07 17:54:00 +0100

    add more SmashProductLie tests and remove old tests

--- test/SmashProductLie-test.jl
+++ test/SmashProductLie-test.jl
@@ -1,7 +1,7 @@
 @testset ExtendedTestSet "All SmashProductLie.jl tests" begin
     
     @testset "smash_product_lie constructor using GAP; R = $R" for R in [QQ, PolynomialRing(QQ, ["x","y","z"])[1]]
-        @testset "$(dynkin)_$n with hw $lambda" for (dynkin, n, lambda) in [('A', 1, [1]), ('A', 2, [1,1]), ('B', 2, [1,0])]
+        @testset "consistency for $(dynkin)_$n with hw $lambda" for (dynkin, n, lambda) in [('A', 1, [1]), ('A', 2, [1,1]), ('B', 2, [1,0])]
             sp, (baseL, baseV) = PD.smash_product_lie(R, dynkin, n, lambda)
             
             @test sp.dimL == length(sp.baseL)
@@ -23,6 +23,9 @@
         @testset "commutators for A_1 with hw [1]" begin
             sp, (baseL, baseV) = PD.smash_product_lie(R, 'A', 1, [1])
             
+            @test sp.dimL == 3
+            @test sp.dimV == 2
+
             x = baseL[1]
             y = baseL[2]
             h = baseL[3]
@@ -32,12 +35,12 @@
             # sl_2 relations
             @test comm(x, x; strict=true) == 0
             @test comm(x, y; strict=true) == h
-            @test comm(x, h; strict=true) == -2x
+            @test comm(x, h; strict=true) == -2*x
             @test comm(y, x; strict=true) == -h
             @test comm(y, y; strict=true) == 0
-            @test comm(y, h; strict=true) == 2y
-            @test comm(h, x; strict=true) == 2x
-            @test comm(h, y; strict=true) == -2y
+            @test comm(y, h; strict=true) == 2*y
+            @test comm(h, x; strict=true) == 2*x
+            @test comm(h, y; strict=true) == -2*y
             @test comm(h, h; strict=true) == 0
 
             # natural representation relations
@@ -47,6 +50,76 @@
             @test comm(y, v2; strict=true) == 0
             @test comm(h, v1; strict=true) == v1
             @test comm(h, v2; strict=true) == -v2
+        end
+
+        @testset "commutators for A_2 with hw [1,1]" begin
+            sp, (baseL, baseV) = PD.smash_product_lie(R, 'A', 2, [1,0])
+            
+            @test sp.dimL == 8
+            @test sp.dimV == 3
+
+            x12 = baseL[1]
+            x23 = -baseL[2] # minus due to representation in GAP 
+            x13 = baseL[3]
+            y12 = baseL[4]
+            y23 = -baseL[5] # minus due to representation in GAP 
+            y13 = baseL[6]
+            h1 = baseL[7]
+            h2 = baseL[8]
+            v1 = baseV[1]
+            v2 = baseV[2]
+            v3 = baseV[3]
+
+            # sl_3 relations
+            @test comm(x12, x23; strict=true) == x13
+            @test comm(x12, x13; strict=true) == 0
+            @test comm(x23, x13; strict=true) == 0
+            @test comm(y12, y23; strict=true) == -y13
+            @test comm(y12, y13; strict=true) == 0
+            @test comm(y23, y13; strict=true) == 0
+            @test comm(h1, h2; strict=true) == 0
+            @test comm(x12, y12; strict=true) == h1
+            @test comm(x23, y23; strict=true) == h2
+            @test comm(x13, y13; strict=true) == h1+h2
+            @test comm(h1, x12; strict=true) == 2*x12
+            @test comm(h1, x23; strict=true) == -x23
+            @test comm(h1, x13; strict=true) == x13
+            @test comm(h1, y12; strict=true) == -2*y12
+            @test comm(h1, y23; strict=true) == y23
+            @test comm(h1, y13; strict=true) == -y13
+            @test comm(h2, x12; strict=true) == -x12
+            @test comm(h2, x23; strict=true) == 2*x23
+            @test comm(h2, x13; strict=true) == x13
+            @test comm(h2, y12; strict=true) == y12
+            @test comm(h2, y23; strict=true) == -2*y23
+            @test comm(h2, y13; strict=true) == -y13
+
+
+            # natural representation relations
+            @test comm(x12, v1; strict=true) == 0
+            @test comm(x23, v1; strict=true) == 0
+            @test comm(x13, v1; strict=true) == 0
+            @test comm(x12, v2; strict=true) == v1
+            @test comm(x23, v2; strict=true) == 0
+            @test comm(x13, v2; strict=true) == 0
+            @test comm(x12, v3; strict=true) == 0
+            @test comm(x23, v3; strict=true) == v2
+            @test comm(x13, v3; strict=true) == v1
+            @test comm(y12, v1; strict=true) == v2
+            @test comm(y23, v1; strict=true) == 0
+            @test comm(y13, v1; strict=true) == v3
+            @test comm(y12, v2; strict=true) == 0
+            @test comm(y23, v2; strict=true) == v3
+            @test comm(y13, v2; strict=true) == 0
+            @test comm(y12, v3; strict=true) == 0
+            @test comm(y23, v3; strict=true) == 0
+            @test comm(y13, v3; strict=true) == 0
+            @test comm(h1, v1; strict=true) == v1
+            @test comm(h2, v1; strict=true) == 0
+            @test comm(h1, v2; strict=true) == -v2
+            @test comm(h2, v2; strict=true) == v2
+            @test comm(h1, v3; strict=true) == 0
+            @test comm(h2, v3; strict=true) == -v3
         end
 
     end
