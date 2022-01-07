980ca696f65f1f2e2748e167e9df57c2ae34f3aa
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2022-01-07 19:17:55 +0100

    reformat docugen file

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,22 @@
 using Documenter
 using PBWDeformations
 
-DocMeta.setdocmeta!(PBWDeformations, :DocTestSetup, :(using PBWDeformations); recursive=true)
+DocMeta.setdocmeta!(
+    PBWDeformations,
+    :DocTestSetup,
+    :(using PBWDeformations);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[PBWDeformations],
-    authors="Johannes Flake <flake@art.rwth-aachen.de>, Lars Göttgens <lars.goettgens@rwth-aachen.de>, Phil Pützstück <phil.puetzstueck@rwth-aachen.de>",
-    # repo="https://gitlab.com/a-rt/PBWDeformations.jl/blob/{commit}{path}#{line}",
-    sitename="PBWDeformations.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://a-rt.gitlab.io/PBWDeformations.jl",
-        assets=String[],
+    modules = [PBWDeformations],
+    repo = "https://gitlab.com/user/project/blob/{commit}{path}#{line}",
+    sitename = "PBWDeformations.jl",
+    checkdocs = :none,
+    strict = true,
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://johannesflake.gitlab.io/PBWDeformations.jl",
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
-    checkdocs=:exports,
-    strict=true,
+    pages = ["Home" => "index.md"],
 )
