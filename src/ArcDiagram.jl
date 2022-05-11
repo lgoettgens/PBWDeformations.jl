275b49377557d87d244deeafcd2fa66728f6153c
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2022-05-11 18:43:56 +0200

    add base generation for so pbwdeforms via arc diagrams

--- src/ArcDiagram.jl
+++ src/ArcDiagram.jl
@@ -86,3 +86,111 @@ function iter_possible_adjacencies(
     end
     return Iterators.flatten(Iterators.map(c -> c[1], choices)), sum(c -> c[2], choices; init=0)
 end
+
+function pbw_arc_diagrams(l::Int, d::Int)
+    indep_sets = [1:l, l+1:2*l, [[2 * l + 2 * i - 1, 2 * l + 2 * i] for i in 1:d]...]
+    return all_arc_diagrams(2 * l, 2 * d; indep_sets)
+end
+
+struct SoDeformArcBase{C <: RingElement} <: DeformBase{C}
+    len::Int
+    iter
+
+    function SoDeformArcBase{C}(sp::SmashProductLie{C}, maxdeg::Int) where {C <: RingElement}
+        dimV = div(isqrt(8 * sp.dimL + 1) + 1, 2) # inverse of gaussian sum
+        l = first(l for l in 1:dimV if binomial(dimV, l) >= sp.dimV)
+
+        d = maxdeg
+        iso_wedge2V_g = Dict{Vector{Int}, Int}()
+        for (i, bs) in enumerate(Combinatorics.combinations(1:dimV, 2))
+            iso_wedge2V_g[bs] = i
+        end
+        index = Dict{Vector{Int}, Int}()
+        for (i, is) in enumerate(Combinatorics.combinations(1:dimV, l))
+            index[is] = i
+        end
+
+        diag_iter = pbw_arc_diagrams(l, d)
+        len = length(diag_iter)
+        iter = (
+            begin
+                kappa = fill(sp.alg(0), sp.dimV, sp.dimV)
+                for is in Combinatorics.combinations(1:dimV, l), js in Combinatorics.combinations(1:dimV, l)
+                    i = index[is]
+                    j = index[js]
+                    if i >= j
+                        continue
+                    end
+                    zeroprod = false
+                    labeled_diag = [is..., js..., [0 for _ in 1:2d]...]
+                    frees = Int[]
+                    for k in 1:length(labeled_diag)
+                        if labeled_diag[k] != 0
+                            if labeled_diag[diag.adjacency[k]] == 0
+                                labeled_diag[diag.adjacency[k]] = labeled_diag[k]
+                            else
+                                if labeled_diag[k] != labeled_diag[diag.adjacency[k]]
+                                    zeroprod = true
+                                    break
+                                end
+                            end
+                        else
+                            if labeled_diag[diag.adjacency[k]] == 0
+                                append!(frees, min(k, diag.adjacency[k]))
+                            end
+                        end
+                    end
+                    if zeroprod
+                        continue
+                    end
+                    unique!(sort!(frees))
+                    free_index = Dict{Int, Int}()
+                    for (k, f) in enumerate(frees)
+                        free_index[f] = k
+                    end
+                    entry = zero(sp.alg)
+                    for labeling in (isempty(frees) ? [Int[]] : AbstractAlgebra.ProductIterator(1:dimV, length(frees)))
+                        lower_labeled = [
+                            labeled_diag[k] != 0 ? labeled_diag[k] : labeling[free_index[min(k, diag.adjacency[k])]] for k in 2*l+1:2*l+2*d
+                        ]
+                        zeroelem = false
+                        sign_pos = true
+                        basiselem = Int[]
+                        for k in 1:2:length(lower_labeled)
+                            if lower_labeled[k] == lower_labeled[k+1]
+                                zeroelem = true
+                                break
+                            elseif lower_labeled[k] > lower_labeled[k+1]
+                                sign_pos = !sign_pos
+                                append!(basiselem, iso_wedge2V_g[[lower_labeled[k+1], lower_labeled[k]]])
+                            else
+                                append!(basiselem, iso_wedge2V_g[[lower_labeled[k], lower_labeled[k+1]]])
+                            end
+                        end
+                        if zeroelem
+                            continue
+                        end
+                        symm_basiselem =
+                            1 // factorial(length(basiselem)) *
+                            sum(prod(sp.baseL[ind]) for ind in Combinatorics.permutations(basiselem))
+                        entry += (sign_pos ? 1 : (-1)) * normal_form(symm_basiselem)
+                    end
+                    kappa[i, j] += entry
+                    kappa[j, i] -= entry
+                end
+                kappa
+            end for diag in diag_iter
+        )
+        return new{C}(len, iter)
+    end
+end
+
+function Base.iterate(i::SoDeformArcBase)
+    return iterate(i.iter)
+end
+
+function Base.iterate(i::SoDeformArcBase, s)
+    return iterate(i.iter, s)
+end
+
+Base.length(base::SoDeformArcBase) = base.len
--- src/PBWDeformations.jl
+++ src/PBWDeformations.jl
@@ -69,11 +69,11 @@ GAP = Oscar.GAP
 
 include("Util.jl")
 include("Algebra.jl")
-include("ArcDiagram.jl")
 include("FreeAlgebra.jl")
 include("QuadraticQuoAlgebra.jl")
 include("SmashProductLie.jl")
 include("SmashProductDeformLie.jl")
+include("ArcDiagram.jl")
 
 
 end
