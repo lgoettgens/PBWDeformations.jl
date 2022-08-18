cd3fb03397914e22a8f64838a1e4c8660470c7c0
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2022-08-18 20:05:31 +0200

    add more structure consts

--- src/LieAlgebraStructConsts.jl
+++ src/LieAlgebraStructConsts.jl
@@ -32,10 +32,53 @@ function liealgebra_standard_module_so_basis(n::Int)
 end
 
 function liealgebra_fundamental_module_so_symbols(n::Int, e::Int)
+    return liealgebra_outpowers_standard_module_so_symbols(n, e)
+end
+
+function liealgebra_fundamental_module_so_struct_const(n::Int, e::Int)
+    return liealgebra_outpowers_standard_module_so_struct_const(n, e)
+end
+
+function liealgebra_symmpowers_standard_module_so_symbols(n::Int, e::Int)
+    return e == 1 ? ["v_$(i)" for i in 1:2n] :
+           ["v_$(js)" for js in Combinatorics.with_replacement_combinations(1:2n, e)]
+end
+
+function liealgebra_symmpowers_standard_module_so_struct_const(n::Int, e::Int) # so_n, e-th symm power of the standard rep
+    basisL = liealgebra_sp_basis(n)
+    basis_standardV = liealgebra_standard_module_sp_basis(n)
+    dimL = length(basisL)
+    if e == 1
+        dimV = n
+        struct_const_V = Matrix{Vector{Tuple{Int, Int}}}(undef, dimL, dimV)
+        for i in 1:dimL, j in 1:dimV
+            struct_const_V[i, j] = [(c, k) for (k, c) in enumerate(basisL[i] * basis_standardV[j]) if !iszero(c)]
+        end
+    elseif e > 1
+        dimV = binomial(e + n - 1, e)
+        struct_const_V = Matrix{Vector{Tuple{Int, Int}}}(undef, dimL, dimV)
+        index = Dict{Vector{Int}, Int}()
+        for (j, js) in enumerate(Combinatorics.with_replacement_combinations(1:n, e))
+            index[js] = j
+        end
+        for i in 1:dimL, js in Combinatorics.with_replacement_combinations(1:n, e)
+            struct_const_V[i, index[js]] = [
+                (c, index[sort([js[1:l-1]; k; js[l+1:end]])]) for l in 1:e for
+                (k, c) in enumerate(basisL[i] * basis_standardV[js[l]]) if !iszero(c)
+            ]
+        end
+    else
+        error("non-negative power")
+    end
+
+    return dimV, struct_const_V
+end
+
+function liealgebra_outpowers_standard_module_so_symbols(n::Int, e::Int)
     return e == 1 ? ["v_$(i)" for i in 1:n] : ["v_$(js)" for js in Combinatorics.combinations(1:n, e)]
 end
 
-function liealgebra_fundamental_module_so_struct_const(n::Int, e::Int) # so_n, e-th outer power of the standard rep
+function liealgebra_outpowers_standard_module_so_struct_const(n::Int, e::Int) # so_n, e-th outer power of the standard rep
     basisL = liealgebra_so_basis(n)
     basis_standardV = liealgebra_standard_module_so_basis(n)
     dimL = length(basisL)
@@ -126,7 +169,6 @@ function liealgebra_symmpowers_standard_module_sp_symbols(n::Int, e::Int)
            ["v_$(js)" for js in Combinatorics.with_replacement_combinations(1:2n, e)]
 end
 
-
 function liealgebra_symmpowers_standard_module_sp_struct_const(n::Int, e::Int) # sp_2n, e-th symm power of the standard rep
     basisL = liealgebra_sp_basis(n)
     basis_standardV = liealgebra_standard_module_sp_basis(n)
@@ -148,6 +190,41 @@ function liealgebra_symmpowers_standard_module_sp_struct_const(n::Int, e::Int) #
             struct_const_V[i, index[js]] = [
                 (c, index[sort([js[1:l-1]; k; js[l+1:end]])]) for l in 1:e for
                 (k, c) in enumerate(basisL[i] * basis_standardV[js[l]]) if !iszero(c)
+            ]
+        end
+    else
+        error("non-negative power")
+    end
+
+    return dimV, struct_const_V
+end
+
+function liealgebra_outpowers_standard_module_sp_symbols(n::Int, e::Int)
+    return e == 1 ? ["v_$(i)" for i in 1:2n] : ["v_$(js)" for js in Combinatorics.combinations(1:2n, e)]
+end
+
+function liealgebra_outpowers_standard_module_sp_struct_const(n::Int, e::Int) # sp_2n, e-th outer power of the standard rep
+    basisL = liealgebra_sp_basis(n)
+    basis_standardV = liealgebra_standard_module_sp_basis(n)
+    dimL = length(basisL)
+    if e == 1
+        dimV = 2n
+        struct_const_V = Matrix{Vector{Tuple{Int, Int}}}(undef, dimL, dimV)
+        for i in 1:dimL, j in 1:dimV
+            struct_const_V[i, j] = [(c, k) for (k, c) in enumerate(basisL[i] * basis_standardV[j]) if !iszero(c)]
+        end
+    elseif e > 1
+        dimV = binomial(2n, e)
+        struct_const_V = Matrix{Vector{Tuple{Int, Int}}}(undef, dimL, dimV)
+        index = Dict{Vector{Int}, Int}()
+        for (j, js) in enumerate(Combinatorics.combinations(1:2n, e))
+            index[js] = j
+        end
+        for i in 1:dimL, js in Combinatorics.combinations(1:2n, e)
+            struct_const_V[i, index[js]] = [
+                (levicivita(sortperm([js[1:l-1]; k; js[l+1:end]])) * c, index[sort([js[1:l-1]; k; js[l+1:end]])])
+                for l in 1:e for (k, c) in enumerate(basisL[i] * basis_standardV[js[l]]) if
+                !iszero(c) && allunique([js[1:l-1]; k; js[l+1:end]])
             ]
         end
     else
