54847c7ef90b3ca1841e8135946050212e2ec8e9
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2022-11-17 18:18:16 +0100

    enhance readability

--- src/DeformationBases/PseudographDeformBasis.jl
+++ src/DeformationBases/PseudographDeformBasis.jl
@@ -57,12 +57,11 @@ function pseudographs_with_partitions__so_extpowers_stdmod(reg::Int, sumtotal::I
     iter = (
         begin
             (pg, Partition(copy(part)))
-        end for sumpg in 0:sumtotal for pg in all_pseudographs(reg, sumpg) for
+        end for sumpg in 0:sumtotal for pg in all_pseudographs(reg, sumpg; upto_iso=true) for
         part in AllParts(sumtotal - sumpg) if all(iseven, part) &&
         all(isodd, pg.loops1) &&
         all(isodd, pg.loops2) &&
-        (pg.loops1 != pg.loops2 || isodd(sum(pg.edges))) &&
-        pg.loops1 <= pg.loops2
+        (pg.loops1 != pg.loops2 || isodd(sum(pg.edges)))
     )
     return collect(iter)
 end
