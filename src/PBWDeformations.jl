664ecc7690bccce19175a2548b44626184afed2a
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-03-24 20:45:53 +0100

    reorder exports

--- src/PBWDeformations.jl
+++ src/PBWDeformations.jl
@@ -43,62 +43,53 @@ import Oscar: action, comm, exterior_power, normal_form, symmetric_power
 
 import Base: deepcopy_internal, hash, isequal, iszero, length, parent, show, sum, zero, +, -, *, ==
 
-export AbstractLieAlgebra,
-    AbstractLieAlgebraElem,
-    ArcDiagram,
-    ArcDiagDeformBasis,
-    DeformationMap,
-    DeformBasis,
-    LieAlgebra,
-    LieAlgebraElem,
-    LieAlgebraModule,
-    LieAlgebraModuleElem,
-    LieAlgebraAbstractModule,
-    LieAlgebraAbstractModuleElem,
-    LieAlgebraExteriorPowerModule,
-    LieAlgebraExteriorPowerModuleElem,
-    LieAlgebraStdModule,
-    LieAlgebraStdModuleElem,
-    LieAlgebraSymmetricPowerModule,
-    LieAlgebraSymmetricPowerModuleElem,
-    LieAlgebraTensorPowerModule,
-    LieAlgebraTensorPowerModuleElem,
-    LinearLieAlgebra,
-    LinearLieAlgebraElem,
-    Pseudograph2,
-    PseudographDeformBasis,
-    QuadraticRelations,
-    SmashProductLie,
-    SmashProductDeformLie,
-    StdDeformBasis
+export AbstractLieAlgebra, AbstractLieAlgebraElem
+export ArcDiagDeformBasis
+export ArcDiagram
+export DeformationMap
+export DeformBasis
+export LieAlgebra, LieAlgebraElem
+export LieAlgebraAbstractModule, LieAlgebraAbstractModuleElem
+export LieAlgebraExteriorPowerModule, LieAlgebraExteriorPowerModuleElem
+export LieAlgebraModule, LieAlgebraModuleElem
+export LieAlgebraStdModule, LieAlgebraStdModuleElem
+export LieAlgebraSymmetricPowerModule, LieAlgebraSymmetricPowerModuleElem
+export LieAlgebraTensorPowerModule, LieAlgebraTensorPowerModuleElem
+export LinearLieAlgebra, LinearLieAlgebraElem
+export Pseudograph2
+export PseudographDeformBasis
+export QuadraticRelations
+export SmashProductDeformLie
+export SmashProductLie
+export StdDeformBasis
 
-export abstract_module,
-    all_arc_diagrams,
-    all_pbwdeformations,
-    all_pseudographs,
-    base_liealgebra,
-    bracket,
-    coefficient_vector,
-    deform,
-    exterior_power,
-    general_linear_liealgebra,
-    highest_weight_module,
-    is_crossing_free,
-    is_pbwdeformation,
-    liealgebra,
-    lookup_data,
-    matrix_repr_basis,
-    nedges,
-    pbwdeform_eqs,
-    smash_product,
-    smash_product_lie_highest_weight,
-    special_linear_liealgebra,
-    special_orthogonal_liealgebra,
-    standard_module,
-    symmetric_deformation,
-    symmetric_power,
-    tensor_power,
-    to_arcdiag
+export abstract_module
+export all_arc_diagrams
+export all_pbwdeformations
+export all_pseudographs
+export base_liealgebra
+export bracket
+export coefficient_vector
+export deform
+export exterior_power
+export general_linear_liealgebra
+export highest_weight_module
+export is_crossing_free
+export is_pbwdeformation
+export liealgebra
+export lookup_data
+export matrix_repr_basis
+export nedges
+export pbwdeform_eqs
+export smash_product
+export smash_product_lie_highest_weight
+export special_linear_liealgebra
+export special_orthogonal_liealgebra
+export standard_module
+export symmetric_deformation
+export symmetric_power
+export tensor_power
+export to_arcdiag
 
 
 GAP = Oscar.GAP
