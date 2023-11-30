b164756e2aac77eb39eff01064a6c13aedd8600f
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-11-30 15:42:50 +0100

    Bump Aqua to 0.8 (#139)

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -4,12 +4,5 @@ using Aqua
     Aqua.test_all(
         PBWDeformations;
         ambiguities=false,          # recursive=false does not work
-        unbound_args=true,
-        undefined_exports=true,
-        project_extras=true,
-        stale_deps=true,
-        deps_compat=true,
-        project_toml_formatting=true,
-        piracy=true,
     )
 end
