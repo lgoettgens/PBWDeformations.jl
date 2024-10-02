8a820c2c972a3e305c93c5b7b3a2669e4f57b5dd
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-10-02 15:02:24 +0200

    Rename todo vars

--- src/SmashProductLie.jl
+++ src/SmashProductLie.jl
@@ -247,21 +247,21 @@ function simplify(e::SmashProductLieElem)
 end
 
 function _normal_form(a::FreeAssAlgElem{C}, rels::Matrix{Union{Nothing, FreeAssAlgElem{C}}}) where {C <: RingElem}
-    todo = deepcopy(a)
-    result = zero(parent(todo))
+    a = deepcopy(a)
+    result = zero(parent(a))
     CR = coefficient_ring(a)
-    A = parent(todo)
-    while todo.length > 0
-        c = leading_coefficient(todo)
-        exp = leading_exponent_word(todo)
-        t = leading_term(todo)
-        todo -= t
+    A = parent(a)
+    while a.length > 0
+        c = leading_coefficient(a)
+        exp = leading_exponent_word(a)
+        t = leading_term(a)
+        a -= t
 
         changed = false
         for i in 1:length(exp)-1
             if exp[i] > exp[i+1] && !isnothing(rels[exp[i], exp[i+1]])
                 changed = true
-                todo += A([c], [exp[1:i-1]]) * rels[exp[i], exp[i+1]] * A([one(CR)], [exp[i+2:end]])
+                a += A([c], [exp[1:i-1]]) * rels[exp[i], exp[i+1]] * A([one(CR)], [exp[i+2:end]])
                 break
             end
         end
