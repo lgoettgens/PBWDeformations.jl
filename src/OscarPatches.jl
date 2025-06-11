b483f7f5abc90fafc085ba2fa0d4f0664701a5a4
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-06-11 13:08:18 +0200

    Bump Oscar compat to 1.4 (#192)

--- src/OscarPatches.jl
+++ src/OscarPatches.jl
@@ -20,14 +20,6 @@ function tensor_power_obj(V::LieAlgebraModule, k::Int)
     return tensor_power(V, k)[1]
 end
 
-if !isdefined(Oscar, :induced_action) # upstreamed in https://github.com/oscar-system/Oscar.jl/pull/4921
-    function induced_action(actfun::Function, phi::Map{<:PermGroup, <:PermGroup})
-        return function (omega, g)
-            return actfun(omega, phi(g))
-        end
-    end
-end
-
 function is_smallest_obj_in_orbit(g::T, G::PermGroup; lt::Function=_lt) where T
     is_fixpoint = true
     for p in gens(G)
