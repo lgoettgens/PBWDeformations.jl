c31468e872fe1a85cebe90988966e3cdcc3fc70c
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-06-12 17:43:31 +0200

    Fix arc diag based deformation bases over number fields (#197)

--- src/DeformationBases/ArcDiagDeformBasis.jl
+++ src/DeformationBases/ArcDiagDeformBasis.jl
@@ -526,7 +526,7 @@ function arcdiag_to_deformationmap_entry(
                 if !iszero(coeff_lower_labels)
                     symm_basiselem = sp(
                         underlying_algebra(sp)(
-                            fill(C(1 // factorial(length(basiselem))), factorial(length(basiselem))),
+                            fill(coefficient_ring(sp)(1 // factorial(length(basiselem))), factorial(length(basiselem))),
                             [ind for ind in permutations(basiselem)],
                         ),
                     ) # TODO: benchmark use of `symmetrize` here once it is implemented with mutable arithmetics
