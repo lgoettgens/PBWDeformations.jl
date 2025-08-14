0e50956976fb7687ba29c23f96f3ac3b3f0085fe
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-08-14 17:16:30 +0200

    Add `is_in_span_with_relation`

--- test/LinearIndependence-test.jl
+++ test/LinearIndependence-test.jl
@@ -29,6 +29,45 @@
 
     end
 
+    @testset "is_in_span_with_relation" begin
+        fl, rel = is_in_span_with_relation(QQ, matrix(QQ, 2, 2, [1, 2, 3, 4]), [matrix(QQ, 2, 2, [1, 2, 3, 4])])
+        @test fl
+        @test rel == [QQ(1)]
+
+        fl, _ = is_in_span_with_relation(QQ, matrix(QQ, 2, 2, [1, 2, 3, 4]), [matrix(QQ, 2, 2, [1, 1, 1, 1])])
+        @test !fl
+
+        fl, rel = is_in_span_with_relation(
+            QQ,
+            matrix(QQ, 2, 2, [1, 2, 3, 4]),
+            [matrix(QQ, 2, 2, [1, 2, 3, 4]), matrix(QQ, 2, 2, [0, 0, 0, 1])],
+        )
+        @test fl
+        @test rel == [QQ(1), QQ(0)]
+
+        fl, _ = is_in_span_with_relation(
+            QQ,
+            matrix(QQ, 2, 2, [1, 2, 3, 4]),
+            [matrix(QQ, 2, 2, [1, 2, 3, 6]), matrix(QQ, 2, 2, [1, 1, 1, 1])],
+        )
+        @test !fl
+
+        fl, rel = is_in_span_with_relation(
+            QQ,
+            matrix(QQ, 2, 2, [1, 2, 3, 4]),
+            [matrix(QQ, 2, 2, [1, 2, 3, 6]), matrix(QQ, 2, 2, [0, 0, 0, 1])],
+        )
+        @test fl
+        @test rel == [QQ(1), QQ(-2)]
+
+        x = matrix(QQ, 2, 2, [1, 2, 3, 4])
+        V = [matrix(QQ, 2, 2, [1, 2, 3, 4]), matrix(QQ, 2, 2, [2, 4, 6, 8])]
+        fl, rel = is_in_span_with_relation(QQ, x, V)
+        @test fl
+        @test sum(c * v for (c, v) in zip(rel, V)) == x
+
+    end
+
     @testset "is_span_subset" begin
         @test is_span_subset(
             QQ,
